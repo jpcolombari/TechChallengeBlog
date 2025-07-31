54c10aef4e61b4ed4bd0a4617b7b24d88dabf13d
João Paulo Colombari <joao.colombari@fcamara.com.br>
2025-07-30 22:41:51 -0300

    Corrigindo redoc

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,21 @@
-export class CreatePostDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreatePostDto {
+  @ApiProperty({
+    description: 'O título da postagem',
+    example: 'O que é Nest.js?',
+  })
+  title: string;
+
+  @ApiProperty({
+    description: 'O conteúdo principal da postagem, em texto.',
+    example: 'Nest.js é um framework Node.js para construir aplicações...',
+  })
+  content: string;
+
+  @ApiProperty({
+    description: 'O nome do autor da postagem.',
+    example: 'João Colombari',
+  })
+  author: string;
+}
