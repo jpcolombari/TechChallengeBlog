35e770963062f1349ade00c2f241f37b5055b99b
João Paulo Colombari <joao.colombari@fcamara.com.br>
2025-09-25 18:46:41 -0300

    Correção de ci/cd

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
+  // Mock (simulação) do AuthService
+  const mockAuthService = {
+    login: jest.fn().mockImplementation((user) => ({
+      access_token: 'mock_jwt_token',
+    })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        // Fornece a simulação no lugar do serviço real
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
