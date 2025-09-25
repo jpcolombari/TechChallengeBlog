cbef07f05afcfa49ed750ff1f1da1ef3aed9e4ad
João Paulo Colombari <joao.colombari@fcamara.com.br>
2025-09-25 18:48:36 -0300

    Correção de ci/cd

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  // Create a fake version (mock) of the UsersService
+  const mockUsersService = {
+    create: jest.fn(dto => {
+      return {
+        _id: 'some-id',
+        ...dto,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        // Provide the fake UsersService to the controller
+        {
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
