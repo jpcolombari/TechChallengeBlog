6c6362e42e1d4f80cb0193e122ecd2a9efe0270a
João Paulo Colombari <joao.colombari@fcamara.com.br>
2026-02-17 20:06:28 -0300

    Adicionando user professor

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import * as bcrypt from 'bcrypt';
@@ -7,8 +7,33 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+export class UsersService implements OnModuleInit {
+  private readonly logger = new Logger(UsersService.name);
+
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
+
+  async onModuleInit() {
+    await this.seedProfessorUser();
+  }
+
+  private async seedProfessorUser() {
+    const email = 'professor@professor.com';
+    const user = await this.userModel.findOne({ email }).exec();
+
+    if (!user) {
+      this.logger.log('Creating default Professor user...');
+      const professorUser = new this.userModel({
+        name: 'Professor',
+        email: email,
+        password: 'Teste@123',
+        role: UserRole.PROFESSOR,
+      });
+      await professorUser.save();
+      this.logger.log('Default Professor user created successfully.');
+    } else {
+      this.logger.log('Default Professor user already exists.');
+    }
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const createdUser = new this.userModel(createUserDto);
